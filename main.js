f8161a74921093017a609979293820d6816a4f35
Akash Eldo <akasheldo@hotmail.com>
2019-05-13 17:45:57 -0400

    Created editDistance1 function This function currently does adding a letter and replacing the initial consonant Updated letter lists to use the same Unicode characters as hangeul.js

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream(path.join(__dirname, "./ko_50k.txt"))
 });
 
-const constants = ['ㄱ','ㄴ','ㄷ','ㄹ','ㅁ','ㅂ','ㅅ','ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ','ㄲ','ㄸ','ㅃ','ㅆ','ㅉ'];
-const vowels = ['ㅏ','ㅑ','ㅓ','ㅕ','ㅗ','ㅛ','ㅜ','ㅠ','ㅡ','ㅣ','ㅐ','ㅒ','ㅔ','ㅖ','ㅘ','ㅙ','ㅚ','ㅝ','ㅞ','ㅟ','ㅢ'];
+const initialConsonants = ['ᄀ','ᄁ','ᄂ','ᄃ','ᄄ','ᄅ','ᄆ','ᄇ','ᄈ','ᄉ','ᄊ','ᄋ','ᄌ','ᄍ','ᄎ','ᄏ','ᄐ','ᄑ','ᄒ'];
+const vowels = ['ᅡ','ᅢ','ᅣ','ᅤ','ᅥ',	'ᅦ','ᅧ','ᅨ','ᅩ',	'ᅪ','ᅫ','ᅬ','ᅭ','ᅮ',	'ᅯ', 'ᅰ','ᅱ',	'ᅲ','ᅳ','ᅴ','ᅵ'];
+const finalConsonants = ['ᆨ','ᆩ','ᆪ','ᆫ','ᆬ',	'ᆭ','ᆮ','ᆯ', 'ᆰ',	'ᆱ','ᆲ','ᆳ','ᆴ',	'ᆵ','ᆶ','ᆷ','ᆸ','ᆹ',	'ᆺ','ᆻ','ᆼ','ᆽ','ᆾ',	'ᆿ','ᇀ','ᇁ','ᇂ'];
 
 function generateFrequencies() {
     return new Promise(function(resolve, reject) {
@@ -23,12 +24,38 @@ function generateFrequencies() {
     });
 }
 
+function editDistance1(word) {
+    // Go through each syllable in word and break into letters
+    let results = [];
+    for(let i = 0;i<word.length;i++) {
+        if(!hangul.is_hangeul(word[i])) {
+            console.log(word[i] + " is not hangul");
+            continue;
+        }
+
+        let spread = hangul.split(word[i]);
+        console.log(spread);
+
+        // Add a letter, if possible
+        if(spread[2] == null) {
+            for(let j = 0;j<finalConsonants.length;j++) {
+                let block = hangul.join(spread[0],spread[1],finalConsonants[j]);
+                results.push(word.substring(0,i) + block + word.substring(i+1));
+            }
+        }
+
+        // Replace a letter
+        // initial constants
+        for(let j = 0;j<initialConsonants.length;j++) {
+            let block = hangul.join(initialConsonants[j],spread[1],spread[2]);
+            results.push(word.substr(0,i) + block + word.substring(i+1))
+        }
+    }
+    return results;
+}
+
 generateFrequencies().then(function(data){
     let query = '내가';
     console.log(data[query]);
-
-    let spreaded = hangul.spread(query);
-    console.log(spreaded);
-    console.log(hangul.split(query[0]));
-
+    console.log(editDistance1(query));
 });
